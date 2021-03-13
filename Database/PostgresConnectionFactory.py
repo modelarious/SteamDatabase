5ea7d6362eef221d9630506a68e4a13b08333131
Michael Hackman <mwhackma@ualberta.ca>
2021-03-12 20:08:16 -0800

    start of websocket

--- Database/PostgresConnectionFactory.py
+++ Database/PostgresConnectionFactory.py
@@ -4,7 +4,7 @@ class PostgresConnectionFactory:
   @staticmethod
   def createConnection():
     conn = psycopg2.connect(
-      database="postgres", user='postgres', password='password', host='127.0.0.1', port= '5432'
+      database="postgres", user='postgres', password='postgres', host='127.0.0.1', port= '5432'
     )
     conn.autocommit = True
     return conn
--- eventlet-server.py
+++ eventlet-server.py
@@ -5,15 +5,67 @@ multiple threads, and graceful code reloading, see:
 http://pypi.python.org/pypi/Spawning/
 """
 
+# import eventlet
+# from eventlet import wsgi
+
+
+# def hello_world(env, start_response):
+#     if env['PATH_INFO'] != '/':
+#         start_response('404 Not Found', [('Content-Type', 'text/plain')])
+#         return ['Not Found\r\n']
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     return ['Hello, World!\r\n']
+
+# wsgi.server(eventlet.listen(('', 5001)), hello_world)
+
+
+from eventlet import wsgi, websocket
 import eventlet
-from eventlet import wsgi
+
+@websocket.WebSocketWSGI
+def hello_world(ws):
+    while True:
+        ws.send("hello world")
+        from_browser = ws.wait()
+        print(from_browser)
+
+class ConnectionStorage:
+    def __init__(self):
+        pass
+
+    def track_game_socket(self, ws):
+        self.game_socket = ws
+    
+    def send_to_game_socket(self, content):
+        self.game_socket.send(str(content))
+
+# def state_updates(ws):
+#     arr = []
+#     for i in range(10):
+#         arr.append(i)
+#         ws.send(str(arr))
+#         print(ws.path)
+#         from_browser = ws.wait()
+#         print(from_browser)
+
+@websocket.WebSocketWSGI
+def socket_collector(ws):
+    connectionStorage.track_game_socket(ws)
+
+def server():
+    # wsgi.server(eventlet.listen(('', 8090)), hello_world)
+    wsgi.server(eventlet.listen(('', 8091)), socket_collector)
+
+from multiprocessing import Process
+from time import sleep
+if __name__ == '__main__':
+    connectionStorage = ConnectionStorage()
+    serverProcess = Process(target=server)
+    serverProcess.start()
 
 
-def hello_world(env, start_response):
-    if env['PATH_INFO'] != '/':
-        start_response('404 Not Found', [('Content-Type', 'text/plain')])
-        return ['Not Found\r\n']
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ['Hello, World!\r\n']
-
-wsgi.server(eventlet.listen(('', 5001)), hello_world)
+    sleep(30)
+    print("writing to socket")
+    connectionStorage.send_to_game_socket('helllo')
+    connectionStorage.send_to_game_socket('hi there')
+    serverProcess.join()
