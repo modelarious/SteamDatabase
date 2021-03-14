2beb7e276d12f56893e51308463bf610d160a500
Michael Hackman <mwhackma@ualberta.ca>
2021-03-14 14:50:11 -0700

    move to queue system - going to refactor

--- eventlet-server.py
+++ eventlet-server.py
@@ -23,55 +23,90 @@ from eventlet import wsgi, websocket
 import eventlet
 from queue import Queue
 
+# provides an interface to access the 
 class SocketInfo:
     def __init__(self, socket):
         self.socket = socket
         self.received_message_queue = Queue()
+    
+    def wait(self):
+        received_message = self.socket.wait()
+        self.received_message_queue.put(received_message)
+        return received_message
 
-# stores a given websocket 
+    def get_message(self):
+        return self.received_message_queue.get()
+    
+    def send_message(self, content):
+        self.socket.send(str(content))
+
+
+
+# XXX
+# The logic here is strange - some deals with the socket itself, other deals with the socket storage
+# perhaps you should move the connection loop into the SocketInfo class and rename it to SocketWrapper
+# XXX
+
 class WebsocketClientHandler:
     def __init__(self):
-        self.sockets = dict()
-        self.received_message_queue = Queue()
+        self.socketInfos = dict()
     
     def track_socket(self, socket, socket_name):
-        self.sockets[socket_name] = socket
+        self.socketInfos[socket_name] = SocketInfo(socket)
         self.__connection_loop(socket_name)
     
+    def _resolve_socket(self, socket_name):
+        return self.socketInfos[socket_name]
+    
     def __connection_loop(self, socket_name):        
-        socket = self.sockets[socket_name]
+        socket = self._resolve_socket(socket_name)
         while True:
             received_message = socket.wait()
-            self.received_message_queue.put(received_message) # XXX this needs to be stored per socket
+
+            # if the client closed the connection, break out
+            if received_message == None:
+                return
             print(received_message)
     
     def send_to_socket(self, socket_name, content):
         print(f"updating {socket_name} with content {content}")
-        self.sockets[socket_name].send(str(content))
+        socket = self._resolve_socket(socket_name)
+        socket.send_message(content)
+    
+    def get_message(self, socket_name):
+        socket = self._resolve_socket(socket_name)
+        return socket.get_message()
+
 
 
 from threading import Thread
 from time import sleep
 if __name__ == '__main__':
-    connectionStorage = ConnectionStorage()
+    websocketClientHandler = WebsocketClientHandler()
 
-    def server(connectionStorage):
+    def server(websocketClientHandler):
 
         # this blocks and fills a queue with messages received from the socket
         @websocket.WebSocketWSGI
         def socket_collector(ws):
             print(ws.path)
-            connectionStorage.track_socket(ws, ws.path)
+            websocketClientHandler.track_socket(ws, ws.path)
 
         wsgi.server(eventlet.listen(('', 8091)), socket_collector)
 
-    serverThread = Thread(target=server, args = (connectionStorage,))
+    serverThread = Thread(target=server, args = (websocketClientHandler,))
     serverThread.start()
-    sleep(30)
+    sleep(10)
     # print("writing to sockets /no and /game")
     # print(f"connectionStorage = {connectionStorage}")
-    connectionStorage.send_to_socket('/game', 'hello')
+    websocketClientHandler.send_to_socket('/game', 'hello')
     # connectionStorage.send_to_game_socket('hi there')
     # connectionStorage.send_to_no_socket('secret secret')
     # connectionStorage.sockets['/input']
+
+    while True:
+        print("top of loop")
+        sleep(5)
+        print(websocketClientHandler.get_message('/game'))
+
     serverThread.join()
