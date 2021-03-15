ea871bc12677b547cbe852360972e1e89eebba19
Michael Hackman <mwhackma@ualberta.ca>
2021-03-14 19:50:54 -0700

    update comments

--- eventlet-server.py
+++ eventlet-server.py
@@ -1,24 +1,3 @@
-"""This is a simple example of running a wsgi application with eventlet.
-For a more fully-featured server which supports multiple processes,
-multiple threads, and graceful code reloading, see:
-
-http://pypi.python.org/pypi/Spawning/
-"""
-
-# import eventlet
-# from eventlet import wsgi
-
-
-# def hello_world(env, start_response):
-#     if env['PATH_INFO'] != '/':
-#         start_response('404 Not Found', [('Content-Type', 'text/plain')])
-#         return ['Not Found\r\n']
-#     start_response('200 OK', [('Content-Type', 'text/plain')])
-#     return ['Hello, World!\r\n']
-
-# wsgi.server(eventlet.listen(('', 5001)), hello_world)
-
-
 from eventlet import wsgi, websocket
 import eventlet
 from queue import Queue
@@ -85,13 +64,11 @@ def server_function():
 
     gameSock.send_message('oh')
     gameSock.send_message('yeah')
-    gameSock.send_message('biiiiiitch')
 
     while True:
         print("top of loop")
         print(gameSock.get_message())
         sleep(5)
-        
 
     serverThread.join()
 
