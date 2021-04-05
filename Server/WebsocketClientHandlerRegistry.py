5318368edd0b51354bc061d7921b1ec63c5d95b8
Michael Hackman <mwhackma@ualberta.ca>
2021-04-05 00:40:41 -0700

    added Event - checkpoint before triggering app through API

--- Server/WebsocketClientHandlerRegistry.py
+++ Server/WebsocketClientHandlerRegistry.py
@@ -1,12 +1,27 @@
 from Server.SocketWrapper import SocketWrapper
+from threading import Event
+from enum import Enum
+
+UPCOMING_STATE = '/upcoming'
+
+expectedSockets = set([
+    UPCOMING_STATE
+])
 
 class WebsocketClientHandlerRegistry:
     def __init__(self):
         self.socketWrappers = dict()
+        self.allSocketsReady = Event()
+
+    def _internalCheckAllSocketsReady(self):
+        if set(self.socketWrappers.keys()) == expectedSockets:
+            #signal to any processes waiting for configuration
+            self.allSocketsReady.set()
     
     def track_socket_and_loop(self, socket, socket_name):
         wrapped_socket = SocketWrapper(socket, socket_name)
         self.socketWrappers[socket_name] = wrapped_socket
+        self._internalCheckAllSocketsReady()
 
         # loop until client closes connection
         wrapped_socket.connection_loop()
