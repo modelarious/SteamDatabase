7557111b9d62366fcf9451744fdd862612d4a090
Michael Hackman <mwhackma@ualberta.ca>
2021-04-16 00:22:23 -0700

    working on next state transition

--- State/StateTracker.py
+++ State/StateTracker.py
@@ -2,7 +2,9 @@ from Server.WebsocketClientHandlerRegistry import WebsocketClientHandlerRegistry
 from State.States import *
 from State.ObserverSocketHookupFactory import ObserverSocketHookupFactory
 
+from QueueEntries.UserInputRequiredQueueEntry import UserInputRequiredQueueEntry
 
+# XXX concurrency
 class StateTracker:
     def __init__(self, websocketRegistry : WebsocketClientHandlerRegistry):
         print("STATETRACKER - waiting on sockets")
@@ -14,6 +16,7 @@ class StateTracker:
 
         self.upcoming  = factoryMethod(UPCOMING_STATE)
         self.findingNameActive = factoryMethod(FINDING_NAME_ACTIVE_STATE)
+        self.awaitingUser = factoryMethod(AWAITING_USER_STATE)
 
     def setUpcomingState(self, gameTitle : str):
         self.upcoming.add(gameTitle)
@@ -21,3 +24,28 @@ class StateTracker:
     def setFindingNameActiveState(self, gameTitle : str):
         self.upcoming.remove(gameTitle)
         self.findingNameActive.add(gameTitle)
+    
+    def setAwaitingUserInputState(self, userInputRequiredEntry : UserInputRequiredQueueEntry):
+        gameTitle = userInputRequiredQueueEntry.getTargetName()
+        self.findingNameActive.remove(gameTitle)
+        self.awaitingUser.addByTag(gameTitle, userInputRequiredQueueEntry.toJson()) 
+    
+
+
+
+# awaiting an available process to start retrieving info
+QUEUED_FOR_INFO_RETRIEVAL_STATE = '/queuedForInfoRetrieval'
+
+# info about game is currently being collected - scraping steam page, hitting steam API
+INFO_RETRIEVAL_ACTIVE_STATE = '/infoRetrievalActive'
+
+# game has been persisted to database and will now show up on main screen
+STORED = '/stored'
+
+# states:
+#  - upcoming
+#  - finding name (active)
+#  - awaiting user input
+#  - queued for info retrieval
+#  - info retrieval (active)
+#  - stored
