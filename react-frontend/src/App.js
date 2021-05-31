36a6615ee0e87d07a350d0cf250a4288c279d77e
Michael Hackman <mwhackma@ualberta.ca>
2021-05-30 17:29:43 -0700

    moved upcoming over to new system

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,36 +1,36 @@
 import React, { Component } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import SocketContainer from "./SocketContainer.js";
-import { FINDING_NAME_ACTIVE_STATE } from './States.js';
+import { FINDING_NAME_ACTIVE_STATE, UPCOMING_STATE } from './States.js';
 
 const autoBind = require('auto-bind');
 
 
-const upcomingSocket = new W3CWebSocket('ws://127.0.0.1:3091/upcoming');
+// const upcomingSocket = new W3CWebSocket('ws://127.0.0.1:3091/upcoming');
 // const findingNameActiveSocket = new W3CWebSocket('ws://127.0.0.1:3091/findingNameActive');
 class App extends Component {
   constructor() {
     super()
     this.state = {}
-    this.state.awaitingUser = [];
+    this.state.upcoming = [];
     this.state.findingNameActive = [];
 		autoBind(this);
   }
   
   componentDidMount() {
-    upcomingSocket.onopen = () => {
-      console.log("/awaitingUser open")
-    };
-    upcomingSocket.onclose = () => {
-      console.log("/awaitingUser close")
-    }
-    upcomingSocket.onmessage = (message) => {
-      console.log(message.data);
-      const receivedMessage = JSON.parse(message.data);
-      this.setState({
-        awaitingUser: receivedMessage
-      });
-    };
+    // upcomingSocket.onopen = () => {
+    //   console.log("/upcoming open")
+    // };
+    // upcomingSocket.onclose = () => {
+    //   console.log("/upcoming close")
+    // }
+    // upcomingSocket.onmessage = (message) => {
+    //   console.log(message.data);
+    //   const receivedMessage = JSON.parse(message.data);
+    //   this.setState({
+    //     upcoming: receivedMessage
+    //   });
+    // };
 
     // findingNameActiveSocket.onopen = () => {
     //   console.log("/findingNameActive open")
@@ -47,6 +47,7 @@ class App extends Component {
     // };
 
 		const endpoints = [
+			"/upcoming",
 			"/findingNameActive",
 			"/infoRetrievalActive",
 			"/stored",
@@ -55,6 +56,20 @@ class App extends Component {
 			"/queuedForInfoRetrieval"
 		];
 		const socketContainer = new SocketContainer(endpoints);
+		socketContainer.sockets[UPCOMING_STATE].onmessage = (message) => {
+      console.log(message.data);
+      const receivedMessage = JSON.parse(message.data);
+      this.setState({
+        upcoming: receivedMessage
+      });
+    };
+
+    socketContainer.sockets[UPCOMING_STATE].onopen = () => {
+      console.log("/UPCOMING_STATE open")
+    };
+    socketContainer.sockets[UPCOMING_STATE].onclose = () => {
+      console.log("/UPCOMING_STATE close")
+    }
 		socketContainer.sockets[FINDING_NAME_ACTIVE_STATE].onmessage = (message) => {
       console.log(message.data);
       const receivedMessage = JSON.parse(message.data);
@@ -62,6 +77,13 @@ class App extends Component {
         findingNameActive: receivedMessage
       });
     };
+
+    socketContainer.sockets[FINDING_NAME_ACTIVE_STATE].onopen = () => {
+      console.log("/findingNameActive open")
+    };
+    socketContainer.sockets[FINDING_NAME_ACTIVE_STATE].onclose = () => {
+      console.log("/upfindingNameActivecoming close")
+    }
 		// console.log(socketContainer.sockets);
 		// console.log("clearing socket states");
 		// this.socketStates = Object.fromEntries( 
@@ -76,10 +98,10 @@ class App extends Component {
 
     <div>
       <div>
-        awaitingUser
+        Upcoming
       </div>
       <div>
-      {this.state.awaitingUser.map(title => (
+      {this.state.upcoming.map(title => (
         <p key={title}>{title}</p>
       ))}
       </div>
