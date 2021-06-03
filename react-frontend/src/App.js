8e2b5fdb512b277110118d8a0eec401a2dd2dbf8
Michael Hackman <mwhackma@ualberta.ca>
2021-06-03 14:46:33 -0700

    removed Board and just did it in pure html

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import Board from 'react-trello'
 import SocketContainer from "./SocketContainer.js";
-import { FINDING_NAME_ACTIVE_STATE, STATES, UPCOMING_STATE, translate_state_to_title } from './States.js';
+import {STATES, 
+  translate_state_to_title, 
+  UPCOMING_STATE,
+  FINDING_NAME_ACTIVE_STATE,
+  AWAITING_USER_STATE,
+  QUEUED_FOR_INFO_RETRIEVAL_STATE,
+  INFO_RETRIEVAL_ACTIVE_STATE,
+  STORED
+} from './States.js';
 
 const autoBind = require('auto-bind');
 
@@ -40,37 +48,37 @@ class App extends Component {
   }
   
   render() {
-    var lanes = [];
-    for (const [stateName, currentSocketArray] of Object.entries({
-    	[UPCOMING_STATE]: this.state[UPCOMING_STATE],
-    	[FINDING_NAME_ACTIVE_STATE]: this.state[FINDING_NAME_ACTIVE_STATE],
-    })) {
-    	var cards = [];
-    	for (const trackedObject of currentSocketArray) {
-    		cards.push({
-    			// id: trackedObject.gameName, 
-    			// title: trackedObject.gameName, 
-    			// description: trackedObject.gameName, 
-    			// label: trackedObject.gameName, 
-    			id: trackedObject, 
-    			title: trackedObject, 
-    			description: trackedObject, 
-    			label: trackedObject, 
-    			draggable: false
-    		})
-    	}
-    	const stateTitle = translate_state_to_title(stateName) 
-    	lanes.push({
-    		id: stateName,
-    		title: stateTitle,
-    		label: stateTitle,
-    		// label: `${cards.length}`,
-    		cards: cards
-    	})
-    }
-    const d = {
-    	'lanes': lanes
-    }
+    // var lanes = [];
+    // for (const [stateName, currentSocketArray] of Object.entries({
+    // 	[UPCOMING_STATE]: this.state[UPCOMING_STATE],
+    // 	[FINDING_NAME_ACTIVE_STATE]: this.state[FINDING_NAME_ACTIVE_STATE],
+    // })) {
+    // 	var cards = [];
+    // 	for (const trackedObject of currentSocketArray) {
+    // 		cards.push({
+    // 			// id: trackedObject.gameName, 
+    // 			// title: trackedObject.gameName, 
+    // 			// description: trackedObject.gameName, 
+    // 			// label: trackedObject.gameName, 
+    // 			id: trackedObject, 
+    // 			title: trackedObject, 
+    // 			description: trackedObject, 
+    // 			label: trackedObject, 
+    // 			draggable: false
+    // 		})
+    // 	}
+    // 	const stateTitle = translate_state_to_title(stateName) 
+    // 	lanes.push({
+    // 		id: stateName,
+    // 		title: stateTitle,
+    // 		label: stateTitle,
+    // 		// label: `${cards.length}`,
+    // 		cards: cards
+    // 	})
+    // }
+    // const d = {
+    // 	'lanes': lanes
+    // }
     
     return (
 
@@ -92,8 +100,46 @@ class App extends Component {
           <p key={title}>{title}</p>
         ))}
       </div>
-      <Board data={d}/>
+      <hr></hr>
+      <div>
+        Awaiting User
+      </div>
+      <div>
+      {this.state[AWAITING_USER_STATE].map(ob => (
+        <p key={ob.gameName}>{ob.gameName}</p>
+      ))}
+      </div>
+      <hr></hr>
+      <div>
+        Queued for Info Retrieval
+      </div>
+      <div>
+        {this.state[QUEUED_FOR_INFO_RETRIEVAL_STATE].map(ob => (
+          <p key={ob.gameNameFromSteam}>{ob.gameNameFromSteam}</p>
+        ))}
+      </div>
+      <hr></hr>
+      <div>
+        Info Retrieval Active
+      </div>
+      <div>
+      {this.state[INFO_RETRIEVAL_ACTIVE_STATE].map(ob => (
+        <p key={ob.gameNameFromSteam}>{ob.gameNameFromSteam}</p>
+      ))}
+      </div>
+      <hr></hr>
+      <div>
+        Stored
+      </div>
+      <div>
+        {this.state[STORED].map(ob => (
+          <p key={ob.name_on_steam}>{ob.name_on_steam}</p>
+        ))}
+      </div>
+      {/* <Board data={d}/> */}
     </div>
+
+
     );
   }
 }
