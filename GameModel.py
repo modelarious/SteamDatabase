2bf905b9f0847b0fcaa405def4cd57f706c9eed4
Michael Hackman <mwhackma@ualberta.ca>
2021-06-07 00:38:30 -0700

    remove name change from other places

--- GameModel.py
+++ GameModel.py
@@ -1,11 +1,11 @@
 class Game:
-    def __init__(self, steam_id, name_on_harddrive, path_on_harddrive, name_on_steam, avg_review_score, user_defined_genres):
+    def __init__(self, steam_id, name_on_harddrive, path_on_harddrive, name_on_steam, avg_review_score, user_defined_tags):
         self.steam_id = steam_id
         self.name_on_harddrive = name_on_harddrive
         self.path_on_harddrive = path_on_harddrive
         self.name_on_steam = name_on_steam
         self.avg_review_score = avg_review_score
-        self.user_defined_genres = user_defined_genres
+        self.user_defined_tags = user_defined_tags
     
     def toDict(self):
         return self.__dict__.copy()
