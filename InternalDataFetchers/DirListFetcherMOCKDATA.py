84c1ed5a534d85cb4f1eef544554a394110b3866
Michael Hackman <mwhackma@ualberta.ca>
2021-07-01 18:28:37 -0700

    filter out games that are already in the database (#37)

--- InternalDataFetchers/DirListFetcherMOCKDATA.py
+++ InternalDataFetchers/DirListFetcherMOCKDATA.py
@@ -1,7 +1,8 @@
 import pickle
+from typing import List
 
 class DirListFetcherMOCKDATA:
-    def get_dirs(self, pathToGamesFolder: str):
+    def get_dirs(self, pathToGamesFolder: str) -> List[str]:
         print("mocking the local games list from directory")
         with open('mockGamesList.txt', 'rb') as mockGamesList:
             gamesOnDisk = pickle.load(mockGamesList)
