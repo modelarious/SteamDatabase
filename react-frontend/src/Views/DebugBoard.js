7833131f5225c69a4b7fbf80572f882fd1dec840
Michael Hackman <mwhackma@ualberta.ca>
2021-07-07 00:49:25 -0700

    compacted somewhat - could probably be improved

--- react-frontend/src/Views/DebugBoard.js
+++ react-frontend/src/Views/DebugBoard.js
@@ -16,71 +16,62 @@ import {
 } from '@chakra-ui/react';
 import CommandButton from '../Interactions/CommandButton';
 
-// XXX now that all objects have game_name_on_disk, you can now improve this code by .map()'ing the Container objects
+const socket_to_name = [
+  {
+    state: UPCOMING_STATE, 
+    name: "Upcoming"
+  },
+  {
+    state: FINDING_NAME_ACTIVE_STATE, 
+    name: "Finding Name Active"
+  },
+  {
+    state: AWAITING_USER_STATE, 
+    name: "Awaiting User"
+  },
+  {
+    state: QUEUED_FOR_INFO_RETRIEVAL_STATE, 
+    name: "Queued for Info Retrieval"
+  },
+  {
+    state: INFO_RETRIEVAL_ACTIVE_STATE, 
+    name: "Info Retrieval Active"
+  },
+  {
+    state: STORED, 
+    name: "Stored"
+  }
+]
+
+// expecting to receive:
+/*
+{
+  commandSocket: A Socket that can be written to,
+  stateData: {
+    [STATE_CONSTANT] : [array of objects currently in this state],
+    [STATE_CONSTANT_2] : [array of objects currently in this state],
+    [STATE_CONSTANT_3] : [array of objects currently in this state],
+    ...
+  }
+}
+*/
 function DebugBoard(props) {
   return <div>
     <ChakraProvider>
       <CommandButton commandSocket={props.commandSocket}/>
-      <SimpleGrid columns={6} spacingX={1} spacingY={1}>
-        <Container>
-          <Text>Upcoming</Text>
+      <SimpleGrid columns={socket_to_name.length} spacingX={1} spacingY={1}> {
+        socket_to_name.map(socket_to_name_data => (
+          <Container>
+          <Text>{socket_to_name_data.name}</Text>
           <br></br>
           <List>
-            {props.stateData[UPCOMING_STATE].map(ob => (
-              <ListItem border="1px solid red" style={{display: 'flex',  borderRadius: '25px', justifyContent:'center', alignItems:'center'}}>{ob.game_name_on_disk}</ListItem>
-            ))} 
-          </List>
-        </Container>
-        
-        <Container>
-          <Text>Finding Name Active</Text>
-          <br></br>
-          <List>
-            {props.stateData[FINDING_NAME_ACTIVE_STATE].map(ob => (
-              <ListItem>{ob.game_name_on_disk}</ListItem>
+            {props.stateData[socket_to_name_data.state].map(state_data => (
+              <ListItem>{state_data.game_name_on_disk}</ListItem>
             ))}
           </List>
-        </Container>
-        
-        <Container>
-          <Text>Awaiting User</Text>
-          <br></br>
-          <List>
-            {props.stateData[AWAITING_USER_STATE].map(ob => (
-              <ListItem>{ob.game_name_on_disk}</ListItem>
-            ))}
-          </List>
-        </Container>
-        
-        <Container>
-          <Text>Queued for Info Retrieval</Text>
-          <br></br>
-          <List>
-            {props.stateData[QUEUED_FOR_INFO_RETRIEVAL_STATE].map(ob => (
-              <ListItem>{ob.game_name_from_steam}</ListItem>
-            ))}
-          </List>
-        </Container>
-        
-        <Container>
-          <Text>Info Retrieval Active</Text>
-          <br></br>
-          <List>
-            {props.stateData[INFO_RETRIEVAL_ACTIVE_STATE].map(ob => (
-              <ListItem>{ob.game_name_from_steam}</ListItem>
-            ))}
-          </List>
-        </Container>
-        
-        <Container>
-          <Text>Stored</Text>
-          <br></br>
-          <List>
-            {props.stateData[STORED].map(ob => (
-              <ListItem>{ob.game_name_from_steam}</ListItem>
-            ))}
-          </List>
-        </Container>
+          </Container>
+        ))
+      }
       </SimpleGrid>
     </ChakraProvider>
   </div>
