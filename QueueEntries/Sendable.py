979c1e65378ed17b2dd01b10d7134abb73b8b163
Michael Hackman <mwhackma@ualberta.ca>
2021-07-09 12:26:24 -0600

    DRY the interface

--- QueueEntries/Sendable.py
+++ QueueEntries/Sendable.py
@@ -3,17 +3,12 @@ from dataclasses import asdict, dataclass
 @dataclass
 class Sendable:
     game_name_on_disk: str
-
     def get_game_name_on_disk(self) -> str:
         return self.game_name_on_disk
 
 @dataclass
-class SteamSendable:
+class SteamSendable(Sendable):
     game_name_on_disk: str
     game_name_from_steam: str
-
-    def get_game_name_on_disk(self) -> str:
-        return self.game_name_on_disk
-    
     def get_game_name_from_steam(self) -> str:
         return self.game_name_from_steam
