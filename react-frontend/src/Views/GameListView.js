04e61c46918674c31d3d18567b6fd96246c3e896
Michael Hackman <mwhackma@ualberta.ca>
2021-09-02 20:53:45 -0700

    refactor

--- react-frontend/src/Views/GameListView.js
+++ react-frontend/src/Views/GameListView.js
@@ -1,52 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import GameView from "./GameView";
+import { Home } from './Home';
 // import { PageTransition } from "@steveeeie/react-page-transition";
 
 const autoBind = require('auto-bind');
-
-
-function Links(props) {
-  return props.games.map((game) => (
-    <Link to={`/games/${game.steam_id}`}>
-      <img
-        alt={`${game.game_name_on_steam} link`}
-        src={game.app_detail.header_image_url}
-      />
-    </Link>
-  ));
-}
-
-class Home extends Component {
-  constructor(props) {
-    super();
-    this.games = props.games;
-    this.updateScrollDistanceMethod = props.updateScrollDistanceMethod;
-    this.currentScrollTop = props.currentScrollTop
-    autoBind(this);
-  }
-
-  // https://stackoverflow.com/questions/53158796/get-scroll-position-with-reactjs
-  componentDidMount() {
-    window.scrollTo(0, this.currentScrollTop)
-    window.addEventListener('scroll', this.listenToScroll)
-  }
-  
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.listenToScroll)
-  }
-  
-  listenToScroll = () => {
-    const winScroll =
-      document.body.scrollTop || document.documentElement.scrollTop
-
-    this.updateScrollDistanceMethod(winScroll)
-  }
-
-  render() {
-    return <Links games={this.games} />;
-  }
-}
 
 class GameListView extends Component {
   constructor(props) {
@@ -56,8 +14,6 @@ class GameListView extends Component {
   }
 
   scrollDistanceUpdate(currentPixelsFromTop) {
-    console.log("ooooooh yeah jerry, I got CALLED!");
-    console.log(currentPixelsFromTop);
     this.pixelsFromTop = currentPixelsFromTop;
   }
 
