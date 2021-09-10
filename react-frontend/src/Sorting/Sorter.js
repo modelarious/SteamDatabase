2e898bceb7a9a882d387e4a8ef9a5151040183db
Michael Hackman <mwhackma@ualberta.ca>
2021-09-10 00:11:56 -0700

    further refactor of sorting mechanism

---
+++ react-frontend/src/Sorting/Sorter.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Dropdown from 'react-dropdown';
+import { ascendingSortingStrategy } from './Strategies/ascendingSortingStrategy';
+
+const autoBind = require('auto-bind');
+
+const ascendingStrategy = new ascendingSortingStrategy();
+const sortingStrategies = [
+  ascendingStrategy
+];
+
+let sortingStrategiesIndex = {}
+for (const strategy of sortingStrategies) {
+  sortingStrategiesIndex[strategy.get_name()] = strategy;
+}
+const sortingStrategyOptions = sortingStrategies.map(
+  strat => strat.get_name()
+)
+const defaultSortStrategyOption = ascendingStrategy.get_name();
+
+const sortingFieldsIndex = {
+  "Steam Game Title": "game_name_from_steam",
+  "Steam Average Review Score": "avg_review_score",
+}
+
+const sortingFieldOptions = Object.keys(sortingFieldsIndex)
+const defaultSortFieldOption = sortingFieldOptions[0];
+
+export class Sorter extends Component {
+  constructor(onUpdate) {
+    super();
+    this.sortingField = sortingFieldsIndex[defaultSortFieldOption];
+    this.sortingStrategy = sortingStrategiesIndex[defaultSortStrategyOption];
+    this.onUpdate = onUpdate;
+    autoBind(this);
+  }
+
+  _onSelectSortStrategy(sortStrategy) {
+    const sortingStrategy = sortStrategy.value;
+    this.sortingStrategy = sortingStrategiesIndex[sortingStrategy];
+    this.onUpdate();
+  }
+
+  _onSelectSortField(sortField) {
+    const sortingField = sortField.value;
+    this.sortingField = sortingFieldsIndex[sortingField];
+    this.onUpdate();
+  }
+
+  getSortedValues(games) {
+    console.log(`sorting by ${this.sortingField}, using ${this.sortingStrategy.get_name()} strategy`);
+    return this.sortingStrategy.sortGames(games, this.sortingField);
+  }
+
+  render() {
+    return <div>
+      <Dropdown options={sortingStrategyOptions} onChange={this._onSelectSortStrategy} value={defaultSortStrategyOption} placeholder="Sort Strategy" />
+      <Dropdown options={sortingFieldOptions} onChange={this._onSelectSortField} value={defaultSortFieldOption} placeholder="Sort Type" />
+    </div>;
+  }
+}
--- react-frontend/src/Views/GameListView.js
+++ react-frontend/src/Views/GameListView.js
@@ -4,73 +4,10 @@ import GameView from "./GameView";
 import { Home } from './Home';
 // import { PageTransition } from "@steveeeie/react-page-transition";
 
-import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-import { ascendingSortingStrategy } from '../Sorting/Strategies/ascendingSortingStrategy';
-
+import { Sorter } from '../Sorting/Sorter';
 const autoBind = require('auto-bind');
 
-const ascendingStrategy = new ascendingSortingStrategy();
-const sortingStrategies = [
-  ascendingStrategy
-];
-
-
-let sortingStrategiesIndex = {}
-for (const strategy of sortingStrategies) {
-  sortingStrategiesIndex[strategy.get_name()] = strategy;
-}
-const sortingStrategyOptions = sortingStrategies.map(
-  strat => strat.get_name()
-)
-const defaultSortStrategyOption = ascendingStrategy.get_name();
-
-
-
-
-
-const sortingFieldsIndex = {
-  "Steam Game Title": "game_name_from_steam",
-  "Steam Average Review Score": "avg_review_score",
-}
-
-const sortingFieldOptions = Object.keys(sortingFieldsIndex)
-const defaultSortFieldOption = sortingFieldOptions[0];
-
-
-class Sorter extends Component {
-  constructor(onUpdate) {
-    super();
-    this.sortingField = sortingFieldsIndex[defaultSortFieldOption];
-    this.sortingStrategy = sortingStrategiesIndex[defaultSortStrategyOption];
-    this.onUpdate = onUpdate;
-    autoBind(this);
-  }
-
-  _onSelectSortStrategy(sortStrategy) {
-    const sortingStrategy = sortStrategy.value;
-    this.sortingStrategy = sortingStrategiesIndex[sortingStrategy];
-    this.onUpdate();
-  }
-
-  _onSelectSortField(sortField) {
-    const sortingField = sortField.value;
-    this.sortingField = sortingFieldsIndex[sortingField];
-    this.onUpdate();
-  }
-  
-  getSortedValues(games) {
-    console.log(`sorting by ${this.sortingField}, using ${this.sortingStrategy.get_name()} strategy`);
-    return this.sortingStrategy.sortGames(games, this.sortingField)
-  }
-
-  render() {
-    return <div>
-      <Dropdown options={sortingStrategyOptions} onChange={this._onSelectSortStrategy} value={defaultSortStrategyOption} placeholder="Sort Strategy" />
-      <Dropdown options={sortingFieldOptions} onChange={this._onSelectSortField} value={defaultSortFieldOption} placeholder="Sort Type" />
-    </div>
-  }
-}
 
 // XXX will this reconstruct when app.state.games has a game added to it?
 class GameListView extends Component {
@@ -94,8 +31,6 @@ class GameListView extends Component {
   _onUpdate() {
     const filtered = this._getFilteredValues(this.state.games);
     const sorted = this.sorter.getSortedValues(filtered);
-    console.log("Sorted games:")
-    console.log(sorted)
     this.setState({
       games: sorted,
     })
